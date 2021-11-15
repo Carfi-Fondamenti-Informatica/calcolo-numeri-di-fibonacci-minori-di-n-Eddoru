1bd1ea049f6b2890f54a89a298a138df689e7933
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-11-15 05:49:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=1, b=0,c=0;
+
+    cin >> n;
+    
+        cout << a << endl;
+        for (c = 0; c < n; c++) {
+            c = b + a;
+            cout << c << endl;
+            b = a;
+            a = c;
+        }
+ 
+return 0;
+
 }
